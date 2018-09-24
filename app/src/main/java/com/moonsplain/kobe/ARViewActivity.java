febda39c62df9d14c12d39464a6e9dbd83ccf2f9
squate <tobogganlad@gmail.com>
2018-09-24 13:31:15 -0400

    in theory added navigation

---
+++ app/src/main/java/com/moonsplain/kobe/ARViewActivity.java
@@ -0,0 +1,13 @@
+package com.moonsplain.kobe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ARViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arview);
+    }
+}
