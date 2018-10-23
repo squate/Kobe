e688f309ab1e19415ed05f61ea41479e9ae6baa8
HungryWolfy <42949663+HungryWolfy@users.noreply.github.com>
2018-10-23 13:12:27 -0400

    Added AR Fragment

--- app/src/main/java/com/moonsplain/kobe/ARViewActivity.java
+++ app/src/main/java/com/moonsplain/kobe/ARViewActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.camera2.*;
 
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+
 public class ARViewActivity extends AppCompatActivity {
 
     @Override
@@ -14,4 +19,15 @@ public class ARViewActivity extends AppCompatActivity {
     }
     //Get intent that started this activity
     Intent intent = getIntent();
+
+    /*MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+            .thenAccept(
+                material -> {
+                    redSphereRenderable =
+                        ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.15f, 0.0f), material); });
+
+    Node node = new Node();
+    node.setParent(arFragment.getArSceneView().getScene());
+    node.setRenderable(andyRenderable); */
+
 }
