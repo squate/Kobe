317663571275e1647f06c43cba70b7672641df35
leahsanta12 <leahsanta12@gmail.com>
2018-12-01 13:25:36 -0500

    Created leaderboard

--- app/src/main/java/com/moonsplain/kobe/GameModule.java
+++ app/src/main/java/com/moonsplain/kobe/GameModule.java
@@ -1,17 +1,17 @@
 package com.moonsplain.kobe;
 
 public class GameModule extends AccelTestActivity{
-    public static Quest[] GAME;
+    //public static Quest[] GAME;
 
     String[] questStrings =
             {"You wake up hanging from your ankles, | Jiggle yourself free: 500-700ms throw, land face-up | you slip out of the shackles onto the coarse stone floor| you get a good core workout but remain bound",
             "E", "h"};
 
-    for (int i = 0; i < questStrings.length;  i++ )
+    //for (int i = 0; i < questStrings.length;  i++ )
 
 
 
-    public boolean attempt(int level, Throw t){
+    /*public boolean attempt(int level, Throw t){
         switch(level){
             case 1: //first quest conditions
                 return (t.a > 500);
@@ -29,5 +29,5 @@ public class GameModule extends AccelTestActivity{
             this.succeedText = s[2];
             this.failText = s[3];
         }
-    }
+    }*/
 }
