ed431d873be116d9ddaae1694a74066c26393a8c
leahsanta12 <leahsanta12@gmail.com>
2018-12-13 18:17:51 -0500

    Commented Main and Help

--- app/src/main/java/com/moonsplain/kobe/HelpActivity.java
+++ app/src/main/java/com/moonsplain/kobe/HelpActivity.java
@@ -1,10 +1,14 @@
+/*
+    Author: Kobe
+    HelpActivity Displays the instructions of how to play Kobe
+ */
 package com.moonsplain.kobe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HelpActivity extends AppCompatActivity {
-
+    //Create activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
